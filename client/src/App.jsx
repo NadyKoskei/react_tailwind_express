bc16a8770535b253f8823f03c6071dd8c8e2bed1
NadyKoskei <cherotichnady@gmail.com>
2026-02-27 17:11:27 +0300

    props

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./index.css";
+import { Button } from "./components/button.jsx";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -11,6 +12,10 @@ function App() {
       <div>
         <p className="text-2xl font-bold text-center">welcome to my app</p>
       </div>
+
+      <Button message="Click Me!" />
+      <Button message="Submit" />
+      <Button message="Send message" />
     </>
   );
 }
--- client/src/components/button.jsx
+++ client/src/components/button.jsx
@@ -1,12 +1,15 @@
 import "../index.css";
 
-function Button ()
+function Button ({message}) 
+//message is a prop (a property that we can pass to a component like an argument to a function)
+//it is in {} expression syntax, which is used to pass dynamic values to a component. It allows us to use JavaScript expressions inside JSX. In this case, we are passing the value of the message prop to the button element, which will be displayed as the button's text.
 {
     return (
         <>
-        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-3">Click Here</button>
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-3">{message}</button>
         </>
     )
 }
 
 export { Button };
+
