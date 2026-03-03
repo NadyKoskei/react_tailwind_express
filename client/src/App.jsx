e60c37e6b3a8680185ea230738ca3761fb36eca3
NadyKoskei <cherotichnady@gmail.com>
2026-03-03 13:23:22 +0300

    page rendering

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,20 +2,15 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./index.css";
-import { Button } from "./components/button.jsx";
+
+import { RegisterPage } from "./Pages/RegisterPage.jsx";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>
-        <p className="text-2xl font-bold text-center">welcome to my app</p>
-      </div>
-
-      <Button message="Click Me!" />
-      <Button message="Submit" />
-      <Button message="Send message" />
+      <RegisterPage />
     </>
   );
 }
