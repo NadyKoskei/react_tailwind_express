f3a20b754d288ca989370f95a00caadc2396b972
NadyKoskei <cherotichnady@gmail.com>
2026-03-04 23:42:11 +0300

    login form

---
+++ client/src/components/login.jsx
@@ -0,0 +1,43 @@
+import "../index.css";
+
+const LogForm = () => {
+  return (
+    <>
+      <div className="pb-4 pl-3 pr-3 mb-10">
+        <h1 className="text-center font-bold text-2xl">LogIn to your account</h1>
+        <div className="flex flex-col justify-center items-center">
+          <form className="border-2 border-gray-100 bg-white p-4 mt-5 mb-5 w-95 rounded-md shadow-xl">
+            <div>
+              <label for="username">UserName:</label>
+              <input
+                type="text"
+                placeholder="Enter your username"
+                className="form-input"
+              />
+            </div>
+
+            <div>
+              <label for="password">Password:</label>
+              <input
+                type="password"
+                placeholder="Enter your password"
+                className="form-input"
+              />
+            </div>
+
+            <div>
+              <button type="submit" className="form-btn">
+                LogIn
+              </button>
+            </div>
+          </form>
+          <p>
+            Don't have an account? <a href="register.html">Register Here</a>
+          </p>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export { LogForm };
