994dba4bd5cf3804f393d5a07ef4a15bb4889c0b
NadyKoskei <cherotichnady@gmail.com>
2026-03-06 18:14:07 +0300

    POST on reg

--- client/src/components/register.jsx
+++ client/src/components/register.jsx
@@ -1,76 +1,153 @@
 import "../index.css";
+import { useState } from "react";
 
 const RegForm = () => {
-    return(
-        <>
-        <div className="pb-4 pl-3 pr-3 mb-10 flex flex-col justify-center items-center">
-            <h1>Register to Charity Minds</h1>
+  // connect input to react state
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    username: "",
+    email: "",
+    phone: "",
+    dob: "",
+    gender: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  //change handler to capture what the user types
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value,
+    });
+  };
+
+  //submit handler to
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    console.log(formData);
+
+    try {
+      const response = await fetch(
+        "https://charity-minds.onrender.com/api/v1/auth/register",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(formData),
+        },
+      );
+
+      const data = await response.json();
+
+      console.log(data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <>
+      <div className="pb-4 pl-3 pr-3 mb-10 flex flex-col justify-center items-center">
+        <h1>Register to Charity Minds</h1>
 
         <form
-          action=""
-          method="post"
+          onSubmit={handleSubmit}
           className="mt-5 mb-5 p-4 bg-white border-2 border-gray-100 rounded-md shadow-xl"
         >
-          
-        <div className="grouped">
-          <div className="form-group">
-            <label for="fName">First Name:</label>
-            <input type="text" id="fName" placeholder="Enter you first name" />
+          <div className="grouped">
+            <div className="form-group">
+              <label htmlFor="firstName">First Name:</label>
+              <input
+                type="text"
+                id="firstName"
+                placeholder="Enter you first name"
+                onChange={handleChange} //connects the input to the state
+              />
+            </div>
+
+            <div className="form-group">
+              <label htmlFor="lastName">Last Name:</label>
+              <input
+                type="text"
+                id="lastName"
+                placeholder="Enter your second name"
+                onChange={handleChange}
+              />
+            </div>
           </div>
 
           <div className="form-group">
-            <label for="lName">Last Name:</label>
-            <input type="text" id="lName" placeholder="Enter your second name" />
-          </div>
-        </div>
-
-        <div className="form-group">
-          <label for="username">Username:</label>
-          <input type="text" id="username" placeholder="Enter your username" />
-        </div>
-
-        <div className="grouped">
-          <div className="form-group">
-            <label for="email">Email:</label>
-            <input type="email" id="email" placeholder="yourmail@gmail.com" />
-          </div>
-
-          <div className="form-group">
-            <label for="phone">Phone:</label>
-            <input type="tel" id="phone" placeholder="+254712345678" />
-          </div>
-        </div>
-
-        <div className="form-group">
-          <label for="dob">Date of Birth:</label>
-          <input type="date" id="dob" />
-        </div>
-
-        <div className="form-group">
-          <label for="gender">Gender:</label>
-          <select id="gender">
-            <option value="0">--Select Gender--</option>
-            <option value="male">Male</option>
-            <option value="female">Female</option>
-            <option value="other">Other</option>
-          </select>
-        </div>
-
-        <div className="grouped">
-          <div className="form-group">
-            <label for="password">Password:</label>
-            <input type="password" id="password" placeholder="**************" />
-          </div>
-
-          <div className="form-group">
-            <label for="confirmPassword">Confirm Password:</label>
+            <label htmlFor="username">Username:</label>
             <input
-              type="password"
-              id="confirmPassword"
-              placeholder="**************"
+              type="text"
+              id="username"
+              placeholder="Enter your username"
+              onChange={handleChange}
             />
           </div>
-        </div>
+
+          <div className="grouped">
+            <div className="form-group">
+              <label htmlFor="email">Email:</label>
+              <input
+                type="email"
+                id="email"
+                placeholder="yourmail@gmail.com"
+                onChange={handleChange}
+              />
+            </div>
+
+            <div className="form-group">
+              <label htmlFor="phone">Phone:</label>
+              <input
+                type="tel"
+                id="phone"
+                placeholder="+254712345678"
+                onChange={handleChange}
+              />
+            </div>
+          </div>
+
+          <div className="form-group">
+            <label htmlFor="dob">Date of Birth:</label>
+            <input type="date" id="dob" onChange={handleChange} />
+          </div>
+
+          <div className="form-group">
+            <label htmlFor="gender">Gender:</label>
+            <select id="gender" onChange={handleChange}>
+              <option value="0">--Select Gender--</option>
+              <option value="male">Male</option>
+              <option value="female">Female</option>
+              <option value="other">Other</option>
+            </select>
+          </div>
+
+          <div className="grouped">
+            <div className="form-group">
+              <label htmlFor="password">Password:</label>
+              <input
+                type="text"
+                id="password"
+                placeholder="**************"
+                onChange={handleChange}
+              />
+            </div>
+
+            <div className="form-group">
+              <label htmlFor="confirmPassword">Confirm Password:</label>
+              <input
+                type="text"
+                id="confirmPassword"
+                placeholder="**************"
+                onChange={handleChange}
+              />
+            </div>
+          </div>
 
           <div id="terms-div">
             <input
@@ -80,7 +157,7 @@ const RegForm = () => {
               required
               id="terms"
             />
-            <label for="terms">I agree to the Terms and Conditions</label>
+            <label htmlFor="terms">I agree to the Terms and Conditions</label>
           </div>
 
           <div id="policy-div">
@@ -91,18 +168,18 @@ const RegForm = () => {
               required
               id="policy"
             />
-            <label for="policy"
-              >I have read and agree to the Privacy Policy</label
-            >
+            <label htmlFor="policy">
+              I have read and agree to the Privacy Policy
+            </label>
           </div>
 
           <div>
             <input type="submit" value="Register" className="form-btn" />
           </div>
         </form>
-        </div>
-        </>
-    );
-}
+      </div>
+    </>
+  );
+};
 
 export { RegForm };
