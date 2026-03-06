2158aa06c58dd2b494466ff0d3171ca162ed2ac8
NadyKoskei <cherotichnady@gmail.com>
2026-03-06 18:11:56 +0300

    links

--- client/src/components/header.jsx
+++ client/src/components/header.jsx
@@ -1,26 +1,23 @@
 import "../index.css";
 import logo from "../assets/images/logo.png";
 
+import { Link } from "react-router-dom";
+
+
 const Header = () => {
   return (
     <>
       <header className="bg-blue-300 px-1 mb-3 shadow-xl">
         <nav className="flex justify-between container mx-auto items-center flex-col py-5 sm:flex-col md:flex-row ">
           <div className="flex flex-row justify-center items-center gap-2">
-            <img src={logo} alt="logo" className="h-10 w-auto"/>
+            <img src={logo} alt="logo" className="h-10 w-auto" />
             <p className="text-lg font-bold">CHARITY MINDS</p>
           </div>
           <div className="flex flex-row">
             <ul className="flex gap-4 text-pink-600 ml-auto">
-              <li>
-                <a href="">Home</a>
-              </li>
-              <li>
-                <a href="">LogIn</a>
-              </li>
-              <li>
-                <a href="">Dashboard</a>
-              </li>
+              <Link to="/login">LogIn</Link>
+              <Link to="/register">Register</Link>
+              <Link to="/admin">Dashboard</Link>
             </ul>
           </div>
         </nav>
