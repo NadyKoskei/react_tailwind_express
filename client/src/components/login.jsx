57e08d0f73a920b740dbb5f75b50a5466e2a73c2
NadyKoskei <cherotichnady@gmail.com>
2026-03-16 17:05:56 +0300

    log in fetch

--- client/src/components/login.jsx
+++ client/src/components/login.jsx
@@ -2,21 +2,52 @@ import "../index.css";
 import { useEffect, useState } from "react";
 
 const LogForm = () => {
+  //storing form values in state, to remember what the user typed
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-  const [users, setUsers] = useState([]);
+  const handleLogin = async (e) => {
+    e.preventDefault();
+
+    console.log(email, password);
+
+    try{
+    const response = await fetch(
+      "https://charity-minds.onrender.com/api/v1/auth/login",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          email: email,
+          password: password,
+        }),
+      },
+    );
+
+    const data = await response.json();
+    console.log(data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   return (
     <>
       <div className="pb-4 pl-3 pr-3 mb-10">
-        <h1 className="text-center font-bold text-2xl">LogIn to your account</h1>
+        <h1 className="text-center font-bold text-2xl">
+          LogIn to your account
+        </h1>
         <div className="flex flex-col justify-center items-center">
-          <form className="border-2 border-gray-100 bg-white p-4 mt-5 mb-5 w-95 rounded-md shadow-xl">
+          <form onSubmit={handleLogin} className="border-2 border-gray-100 bg-white p-4 mt-5 mb-5 w-95 rounded-md shadow-xl">
             <div>
-              <label htmlFor="username">UserName:</label>
+              <label htmlFor="email">Email:</label>
               <input
                 type="text"
-                placeholder="Enter your username"
+                placeholder="Enter your email"
                 className="form-input"
+                onChange={(e) => setEmail(e.target.value)}
               />
             </div>
 
@@ -26,6 +57,7 @@ const LogForm = () => {
                 type="password"
                 placeholder="Enter your password"
                 className="form-input"
+                onChange={(e) => setPassword(e.target.value)}
               />
             </div>
 
